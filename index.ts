2c568ae0e35dc183cdf0c3a426602a3443e595f4
FlameWolf <agnijith@gmail.com>
2022-07-18 18:40:32 +0530

    Fix CORS configuration

--- index.ts
+++ index.ts
@@ -25,12 +25,14 @@ require("cloudinary").v2.config({
 
 const server = fastify();
 server.register(require("@fastify/helmet"));
-server.addHook("onRequest", (request, reply, done) => {
+server.addHook("onRequest", async (request, reply) => {
 	reply.header("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN);
 	reply.header("Access-Control-Allow-Credentials", true);
 	reply.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Slug, X-UID");
 	reply.header("Access-Control-Allow-Methods", "OPTIONS, POST, PUT, PATCH, GET, DELETE");
-	done();
+	if (request.method === "OPTIONS") {
+		reply.send();
+	}
 });
 server.register(require("fastify-multer").contentParser);
 if (!isProdEnv) {
