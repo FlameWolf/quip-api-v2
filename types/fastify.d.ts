abc6668963912a529ade0b578eff931ea5583efb
FlameWolf <agnijith@gmail.com>
2022-07-29 18:07:27 +0530

    Refactor code

--- types/fastify.d.ts
+++ types/fastify.d.ts
@@ -1,13 +1,13 @@
 "use strict";
 
 import fastify, { FastifyRequest, FastifySchema } from "fastify";
-import { File } from "fastify-multer/lib/interfaces";
+import { File as MulterFile } from "fastify-multer/lib/interfaces";
 import { JwtPayload } from "jsonwebtoken";
 
 declare module "fastify" {
 	interface FastifyRequest {
 		userInfo?: string | JwtPayload | UserInfo;
-		file?: File;
+		file?: MulterFile;
 		fileType?: string;
 		fileSubtype?: string;
 	}
