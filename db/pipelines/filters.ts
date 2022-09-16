6c55023cf1349956814d48293feeb5b29e66864e
FlameWolf <agnijith@gmail.com>
2022-09-16 17:45:40 +0530

    Use cached filters from user entity in filter pipeline

--- db/pipelines/filters.ts
+++ db/pipelines/filters.ts
@@ -7,183 +7,28 @@ const filtersAggregationPipeline = (userId?: string | ObjectId): Array<PipelineS
 	if (!userId) {
 		return [];
 	}
-	const userObjectId = new ObjectId(userId);
 	return [
 		{
 			$lookup: {
-				from: "blocks",
+				from: "users",
 				pipeline: [
 					{
 						$match: {
-							$expr: {
-								$eq: ["$blockedBy", userObjectId]
-							}
-						}
-					},
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$user"
-							}
-						}
-					}
-				],
-				as: "blockedUsers"
-			}
-		},
-		{
-			$addFields: {
-				blockedUsers: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$blockedUsers.result", 0]
+							_id: new ObjectId(userId)
 						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$lookup: {
-				from: "mutedusers",
-				pipeline: [
-					{
-						$match: {
-							$expr: {
-								$eq: ["$mutedBy", userObjectId]
-							}
-						}
-					},
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$user"
-							}
-						}
-					}
-				],
-				as: "mutedUsers"
-			}
-		},
-		{
-			$addFields: {
-				mutedUsers: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedUsers.result", 0]
-						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$lookup: {
-				from: "mutedposts",
-				pipeline: [
-					{
-						$match: {
-							$expr: {
-								$eq: ["$mutedBy", userObjectId]
-							}
-						}
-					},
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$post"
-							}
-						}
-					}
-				],
-				as: "mutedPosts"
-			}
-		},
-		{
-			$addFields: {
-				mutedPosts: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedPosts.result", 0]
-						},
-						[]
-					]
-				}
-			}
-		},
-		{
-			$lookup: {
-				from: "mutedwords",
-				pipeline: [
-					{
-						$match: {
-							$expr: {
-								$eq: ["$mutedBy", userObjectId]
-							}
-						}
-					},
-					{
 						$project: {
-							_id: 0,
-							regEx: {
-								$switch: {
-									branches: [
-										{
-											case: {
-												$eq: ["$match", "startsWith"]
-											},
-											then: {
-												$concat: ["\\b", "$word", ".*?\\b"]
-											}
-										},
-										{
-											case: {
-												$eq: ["$match", "endsWith"]
-											},
-											then: {
-												$concat: ["\\b\\w*?", "$word", "\\b"]
-											}
-										},
-										{
-											case: {
-												$eq: ["$match", "exact"]
-											},
-											then: {
-												$concat: ["\\b", "$word", "\\b"]
-											}
-										}
-									],
-									default: "$word"
-								}
-							}
-						}
-					},
-					{
-						$group: {
-							_id: undefined,
-							result: {
-								$addToSet: "$regEx"
-							}
+							blockedUsers: 1,
+							mutedUsers: 1,
+							mutedPosts: 1,
+							mutedWords: 1
 						}
 					}
 				],
-				as: "mutedWords"
+				as: "activeUser"
 			}
 		},
 		{
-			$addFields: {
-				mutedWords: {
-					$ifNull: [
-						{
-							$arrayElemAt: ["$mutedWords.result", 0]
-						},
-						[]
-					]
-				}
-			}
+			$unwind: "$activeUser"
 		},
 		{
 			$match: {
@@ -191,31 +36,31 @@ const filtersAggregationPipeline = (userId?: string | ObjectId): Array<PipelineS
 					$and: [
 						{
 							$not: {
-								$in: ["$author", "$blockedUsers"]
+								$in: ["$author", "$activeUser.blockedUsers"]
 							}
 						},
 						{
 							$not: {
 								$or: [
 									{
-										$in: ["$author", "$mutedUsers"]
+										$in: ["$author", "$activeUser.mutedUsers"]
 									},
 									{
-										$in: ["$repeatedBy", "$mutedUsers"]
+										$in: ["$repeatedBy", "$activeUser.mutedUsers"]
 									}
 								]
 							}
 						},
 						{
 							$not: {
-								$in: ["$_id", "$mutedPosts"]
+								$in: ["$_id", "$activeUser.mutedPosts"]
 							}
 						},
 						{
 							$eq: [
 								{
 									$filter: {
-										input: "$mutedWords",
+										input: "$activeUser.mutedWords",
 										cond: {
 											$regexMatch: {
 												input: "$content",
@@ -233,7 +78,7 @@ const filtersAggregationPipeline = (userId?: string | ObjectId): Array<PipelineS
 			}
 		},
 		{
-			$unset: ["blockedUsers", "mutedUsers", "mutedPosts", "mutedWords"]
+			$unset: "activeUser"
 		}
 	];
 };
