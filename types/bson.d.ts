fae985f9e24c997b37c8804e0e7f1113178a391a
FlameWolf <agnijith@gmail.com>
2023-11-18 14:31:43 +0530

    Update dependencies

---
+++ types/bson.d.ts
@@ -0,0 +1,8 @@
+declare module "bson" {
+	declare class ObjectId {
+		_id: this;
+		constructor(param: any = undefined) {
+			return param ? new ObjectId() : ObjectId.createFromHexString(param);
+		}
+	}
+}
